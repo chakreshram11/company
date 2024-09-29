8f19984578d674581f82ffa8353556526151f5fe
chakreshram11 <chakreshram11@gmail.com>
2024-09-29 23:53:57 +0530

    installed tailwind css

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}", // Adjust the path according to your project structure
+  ],
+  theme: {
+    extend: {
+      
+      keyframes: {
+        letterReveal: {
+          '0%': { opacity: 0, transform: 'translateY(100%)' },
+          '100%': { opacity: 1, transform: 'translateY(0)' },
+        },
+        scroll: {
+          '0%': { transform: 'translateX(0)' },
+          '100%': { transform: 'translateX(-100%)' }, // Adjust this value to match the total width of your content
+        },
+      },
+      animation: {
+        letterReveal: 'letterReveal 0.5s ease forwards',
+        scroll: 'scroll 20s linear infinite',
+      },
+    },
+  },
+  plugins: [],
+};
